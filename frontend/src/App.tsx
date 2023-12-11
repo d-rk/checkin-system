d2319bd5baae52a44872823f7576335c3fbd84e2
Dirk Wilden <dirkwilden@gmail.com>
2023-12-11 10:57:23 +0100

    update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import {Box} from '@chakra-ui/react';
-import React from 'react';
 import {Outlet} from 'react-router-dom';
 import Header from './components/header/header';
 export const App = () => {
