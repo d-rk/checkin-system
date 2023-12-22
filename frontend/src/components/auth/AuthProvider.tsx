892f873b78a31a02496f85f15423729e7d058f97
Dirk Wilden <dirkwilden@gmail.com>
2023-12-22 12:41:48 +0100

    wait for login to complete

--- frontend/src/components/auth/AuthProvider.tsx
+++ frontend/src/components/auth/AuthProvider.tsx
@@ -66,6 +66,7 @@ export const AuthProvider = ({children}: {children: React.ReactNode}) => {
       localStorage.setItem('token', token);
     }
 
+    setUserLoading(true);
     setToken(token);
   };
 
